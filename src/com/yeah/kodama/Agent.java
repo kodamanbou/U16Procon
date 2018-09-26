3879a52e8ecf2dca09f3f93ccd31c38b46ba2dac
kodamanbou <kodamanbou0424@gmail.com>
2018-09-26 20:27:03 +0900

    全然わからん

--- src/com/yeah/kodama/Agent.java
+++ src/com/yeah/kodama/Agent.java
@@ -1,10 +1,20 @@
 package com.yeah.kodama;
 
+import java.awt.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Agent {
 
     private double q[][][] = new double[17][15][16];
+    private HashMap<Point, Integer> qmap = new HashMap<>();
     private double gamma = 0.90;
     private double learning_rate = 0.10;
+    private double epsilon = 0.30;
+
+    private final int HIT_WALL_PENALTY = 100;           //ペナルティはあとで調整が必要
+    private final int ENEMY_ENCOUNTER_PENALTY = 100;
+    private final int GET_ITEM_REWARD = 10;
+    private final int ONE_STEP_PENALTY = 1;
+
 }
--- src/com/yeah/kodama/Environment.java
+++ src/com/yeah/kodama/Environment.java
@@ -1,15 +1,9 @@
 package com.yeah.kodama;
 
 public final class Environment {
-    public static final int FLOOR = 0;  //各定数を、それぞれのアクションを取った時の報酬(Rewards)とする。
-    public static final int ENEMY = 1;
-    public static final int WALL = 2;
-    public static final int ITEM = 3;
-
-    private int[][] state = new int[17][15];
 
     private static Environment theInstance;
-    private Environment() {}
+    private Environment() {}    //シングルトンパターン
 
     public static Environment getInstance() {
         if (theInstance == null) {
