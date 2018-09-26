3eb91c1a646fd0b1cf07bd8c9d16f1326901ff30
kodamanbou <kodamanbou0424@gmail.com>
2018-09-27 01:27:08 +0900

    いえあ

--- src/com/yeah/kodama/Agent.java
+++ src/com/yeah/kodama/Agent.java
@@ -6,15 +6,38 @@ import java.util.HashMap;
 
 public class Agent {
 
-    private double q[][][] = new double[17][15][16];
-    private HashMap<Point, Integer> qmap = new HashMap<>();
-    private double gamma = 0.90;
-    private double learning_rate = 0.10;
+    private HashMap<Point, Action> qmap = new HashMap<>();
+    private double gamma = 0.99;
+    private double learning_rate = 0.5;
     private double epsilon = 0.30;
+    private double total_reward = 0;
 
     private final int HIT_WALL_PENALTY = 100;           //ペナルティはあとで調整
     private final int ENEMY_ENCOUNTER_PENALTY = 100;
-    private final int GET_ITEM_REWARD = 10;
     private final int ONE_STEP_PENALTY = 1;
+    private final int ENEMY_DEFEAT_REWARD = 100;
+    private final int GET_ITEM_REWARD = 10;
+
+    private enum Action {
+        WalkUp,
+        WalkRight,
+        WalkLeft,
+        WalkDown,
+        LookUp,
+        LookRight,
+        LookLeft,
+        LookDown,
+        SearchUp,
+        SearchRight,
+        SearchLeft,
+        SearchDown,
+        PutUp,
+        PutRight,
+        PutLeft,
+        PutDown
+    }
+
+    public Agent(Point origin) {
+    }
 
 }
