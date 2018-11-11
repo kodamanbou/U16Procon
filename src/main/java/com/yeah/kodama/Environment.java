ef14a8be096a62130d00c805815fb716f9e441c9
kodamanbou <kodamanbou0424@gmail.com>
2018-11-12 08:13:03 +0900

    迷走中.

--- src/main/java/com/yeah/kodama/Environment.java
+++ src/main/java/com/yeah/kodama/Environment.java
@@ -41,10 +41,11 @@ public final class Environment implements Game {
         Random random = new Random(System.currentTimeMillis());
         sample[8][7] = 3;
         current = new Point(random.nextInt(7), random.nextInt(8));      //現在地を設定.
-        for (int i = 0; i < 17; i++) {
-            for (int j = 0; j < 15; j++) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 8; j++) {
                 //Map
                 if (current.x != j || current.y != i) {
+                    sample[i][j] = 2;
                 }
             }
         }
